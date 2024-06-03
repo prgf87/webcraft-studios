d48fb5fa8c0d8e497347216c82e21b83e4a770c3
joshnevard <joshnevard@gmail.com>
2024-06-03 19:43:28 +0100

    migration from original repo

---
+++ src/components/header.js
@@ -0,0 +1,112 @@
+import Image from 'next/image';
+import logo from '../../public/images/logo/logo.png';
+import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleHeader = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="sticky top-0 w-full h-20 shadow-xl bg-white">
+      <div className="max-w-[1600px] mx-auto h-full px-8">
+        <div className="flex justify-between items-center h-full w-full ">
+          <Link className="flex items-center ml-4" href="/#home">
+            <Image
+              src={logo}
+              alt="logo"
+              width={20}
+              height={20}
+              className="cursor-pointer rounded-sm mr-3"
+              priority
+            />
+            <h1 className="font-bold">Webcraft Studios</h1>
+          </Link>
+
+          <div className="hidden text-sm sm:flex mr-5">
+            <ul className="hidden sm:flex space-x-10">
+              <Link href="/#services">
+                <li>Services</li>
+              </Link>
+              <Link href="/#portfolio">
+                <li>Portfolio</li>
+              </Link>
+              <Link href="/#about">
+                <li>About</li>
+              </Link>
+              <Link href="/#contact">
+                <li>Contact</li>
+              </Link>
+            </ul>
+          </div>
+          <div
+            onClick={handleHeader}
+            className="sm:hidden cursor-pointer mr-2 pl-24"
+          >
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+        <div
+          className={`sm:hidden h-screen ease-in duration-500 p-10 ${
+            menuOpen
+              ? 'fixed left-0 top-0 w-[65%]  bg-[#ecf0f3] '
+              : 'fixed left-[-100%] top-0'
+          }`}
+        >
+          <div className="flex w-full items-center justify-end">
+            <div onClick={handleHeader} className="cursor-pointer">
+              <AiOutlineClose size={25} />
+            </div>
+          </div>
+          <div className="flex-col py-4">
+            <ul>
+              <Link href="/#home">
+                <li
+                  onClick={() => setMenuOpen(false)}
+                  className="py-4 cursor-pointer"
+                >
+                  Home
+                </li>
+              </Link>
+              <Link href="/#services">
+                <li
+                  onClick={() => setMenuOpen(false)}
+                  className="py-4 cursor-pointer"
+                >
+                  Services
+                </li>
+              </Link>
+              <Link href="/#portfolio">
+                <li
+                  onClick={() => setMenuOpen(false)}
+                  className="py-4 cursor-pointer"
+                >
+                  Portfolio
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li
+                  onClick={() => setMenuOpen(false)}
+                  className="py-4 cursor-pointer"
+                >
+                  About
+                </li>
+              </Link>
+              <Link href="/#contact">
+                <li
+                  onClick={() => setMenuOpen(false)}
+                  className="py-4 cursor-pointer"
+                >
+                  Contact
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/components/hero.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import pic from '../../public/images/4.jpg';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Hero() {
+  return (
+    <div className="max-w-xl sm:max-w-5xl mx-auto py-10">
+      <div className="grid lg:grid-cols-2 mx-auto ">
+        <div className="flex justify-start items-center p-4">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-1 gap-x-8">
+            <h1 className="text-4xl font-bold pb-4 sm:text-right lg:text-left">
+              Let WebCraft Studios{' '}
+              <span className="text-[#7D1CBF]">Elevate</span> Your Online
+              Presence
+            </h1>
+            <div>
+              <p>
+                We are a team of passionate designers and developers dedicated
+                to creating stunning, user-friendly websites that drive results
+                for your business.
+              </p>
+              <div className="pt-4">
+                <Link href={'/#about-us'}>
+                  <button className="primary-button">Find Out More</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto p-4">
+          <Image
+            src={pic}
+            alt="picture"
+            width={5184}
+            height={3456}
+            className="rounded-xl max-h-[35dvh] w-[100dvw] object-cover object-center"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
