469150fdd5a91feefae11ab627505a45e1b6cc21
joshnevard <joshnevard@gmail.com>
2024-06-26 14:27:53 +0100

    faq review

--- src/components/faq.js
+++ src/components/faq.js
@@ -57,17 +57,18 @@ export default function FAQ() {
               className="flex justify-between items-center w-full py-4 px-4 focus:outline-none"
               onClick={() => toggleFAQ(index)}
             >
-              <span className="text-base sm:text-lg font-medium">
+              <span className="text-base sm:text-lg font-medium truncate">
                 {faq.title}
               </span>
               <svg
-                className={`w-8 h-8 sm:w-6 sm:h-6 transition-transform duration-300 ${
+                className={`w-6 h-6 sm:w-8 sm:h-8 transition-transform duration-300 ${
                   isOpen ? 'transform rotate-180' : ''
                 }`}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="xMidYMid meet"
               >
                 <path
                   strokeLinecap="round"
