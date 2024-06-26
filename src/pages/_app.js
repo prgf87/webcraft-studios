c698d1428a995c7f99ada93089cbea711b4edd0a
Pedro <prgf2011@gmail.com>
2024-06-26 14:05:47 +0100

    creates a provider to wrap around the app to provide theme toggle

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { Providers } from '@/components/providers';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  );
 }
